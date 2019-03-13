88cefe245731cb2a525221a24834ec37585db8f2
Néfix Estrada <nefixestrada@gmail.com>
2019-03-13 14:53:34 +0100

    Created the Login page
    Also done other things, but... who cares? ;)

--- src/new_webapp/frontend/src/router.js
+++ src/new_webapp/frontend/src/router.js
@@ -1,23 +1,46 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Home from './views/Home.vue'
+import Login from './views/Login.vue'
 
 Vue.use(Router)
 
-export default new Router({
+var router = new Router({
   routes: [
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/login',
+      name: 'login',
+      component: Login,
+      meta: {
+        showNavBar: false
+      }
     }
   ]
 })
+
+var isLogged = false
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (isLogged === false) {
+      next({
+        path: '/login'
+      })
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
+})
+
+export default router
---
+++ src/new_webapp/frontend/src/views/Login.vue
@@ -0,0 +1,74 @@
+<template>
+    <b-row id="login" align-h="center">
+        <b-col sm="10" md="6" lg="5" xl="4">
+            <img src="../assets/logo_black.png" alt="IsardVDI logo (an Isard head icon)" />
+
+            <h1>IsardVDI Login</h1>
+
+            <b-form @submit="login">
+                <b-form-input
+                    id="username"
+                    type="text"
+                    required
+                    v-model="form.username"
+                    autofocus
+                    placeholder="Username" />
+
+                <b-form-input
+                    id="password"
+                    type="password"
+                    required
+                    v-model="form.password"
+                    placeholder="Password" />
+
+                <b-button type="submit">Login</b-button>
+            </b-form>
+        </b-col>
+    </b-row>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      form: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    login (event) {
+      event.preventDefault()
+      console.log('Logged in!')
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#login {
+    // Position
+    margin-top: 100px;
+    text-align: center;
+
+    img {
+        // Size
+        height: 150px;
+
+        // Position
+        margin-bottom: 25px;
+    }
+
+    form {
+        // Position
+        margin: 25px;
+
+        input {
+            // Position
+            margin-bottom: 18px;
+        }
+    }
+}
+</style>
