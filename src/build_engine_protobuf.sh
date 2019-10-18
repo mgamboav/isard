41505cf83d7a62441f74a432b89a9eb762e26997
jvinolas <vinolasbcn@gmail.com>
2019-10-18 11:12:21 +0200

    revert to working commit

--- src/build_engine_protobuf.sh
+++ src/build_engine_protobuf.sh
@@ -1,4 +1,4 @@
-python3 -m grpc_tools.protoc --proto_path=. ./engine/grpc/proto/desktops.proto --python_out=. --grpc_python_out=.
+python3 -m grpc_tools.protoc --proto_path=. ./engine/grpc/proto/desktop.proto --python_out=. --grpc_python_out=.
 python3 -m grpc_tools.protoc --proto_path=. ./engine/grpc/proto/desktops_stream.proto --python_out=. --grpc_python_out=.
 python3 -m grpc_tools.protoc --proto_path=. ./engine/grpc/proto/templates.proto --python_out=. --grpc_python_out=.
 python3 -m grpc_tools.protoc --proto_path=. ./engine/grpc/proto/templates_stream.proto --python_out=. --grpc_python_out=.
