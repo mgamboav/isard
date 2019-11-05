6d2a6f9a6b523333250cf01ef58ae349be446557
beto <alarraz@escoladeltreball.org>
2019-11-05 09:42:45 +0100

    developing hyp

---
+++ src/engine2/core/hyp.py
@@ -0,0 +1,66 @@
+# Copyright 2019 the Isard-vdi project authors:
+#      Alberto Larraz Dalmases
+#      Josep Maria Viñolas Auquer
+# License: AGPLv3
+from engine2.common.exceptions import UnAcceptedValueConnectionHypParameters
+
+import libvirt
+
+class Hyp(object):
+    """Operates with libvirt hypervisor
+
+    Try connect with ssh with detailed error, create connexions, register stats.
+
+    Args:
+        hostname (str): valid hostname to connect via ssh.
+        username (Optional[str]): Defaults to 'root'.
+        port (Optional[int]): Defaults to 22.
+
+    Attributes:
+        conn: Libvirt connection if established
+
+    """
+    def __init__(self, hostname: str, username: str = 'root', port: int = 22):
+        """Try to connect to hypervisor
+        Raises:
+            UnAcceptedValueConnectionHypParameters: if port or hostname are invalid"""
+
+        if 1 < port < pow(2, 16):
+            port = int(port)
+        else:
+            log.error("port to connect hypervisor {} is not valid: {port}")
+            raise UnAcceptedValueConnectionHypParameters("Port innvalid, must be between 1 and 2^16: {port}")
+
+        if (type(port) == int) and port > 1 and port < pow(2, 16):
+            log.error("port to connect hypervisor {} is not valid: {port}")
+            raise UnAcceptedValueConnectionHypParameters("Port innvalid: {port}")
+
+        self.port = int(port)
+
+    def verify_parameters_ssh(self,port,hostname,username):
+        if type(port) is not int:
+            raise UnAcceptedValueConnectionHypParameters("Port for ssh connection must be integer")
+        if type(address) is not str:
+            raise UnAcceptedValueConnectionHypParameters("Hostname for ssh connection must be string")
+        if type(username) is not str:
+            raise UnAcceptedValueConnectionHypParameters("Usename for ssh connection must be string")
+
+        #port between 1 and 2^16
+        if 1 < port < pow(2, 16):
+            port = int(port)
+        else:
+            log.error("port to connect hypervisor {} is not valid: {port}")
+            raise UnAcceptedValueConnectionHypParameters("Port innvalid, must be between 1 and 2^16: {port}")
+
+        #test if hostame is valid
+        if hostname[-1] == ".":
+            hostname = hostname[:-1]  # strip exactly one dot from the right, if present
+        #allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
+        #if all(allowed.match(x) for x in hostname.split(".")) is False:
+        if all(x.find(' ')<0 for x in hostname.split('.')):
+            raise UnAcceptedValueConnectionHypParameters(f"Hostname as space characters: {hostname}")
+
+        if all(x.find(' ')<0 for x in username.split('.')):
+            raise UnAcceptedValueConnectionHypParameters(f"Username as space characters: {username}")
+
+
